1b7b11cb35ae71327430de32f3a01bf12791fd88
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-05-06 22:50:56 +0530

    #6 Alternate Flavour

---
+++ 6-Alternative_Flavour.js
@@ -0,0 +1,9 @@
+module.exports.items = [ 'item1', 'item2' ];
+//? Here the syntax is module.exports.var_name = arrey of values.
+
+const person = {
+	name: 'Deep'
+};
+
+module.exports.singlePerson = person;
+//? Similarly module.exports.var_name = alloc value of another var.
